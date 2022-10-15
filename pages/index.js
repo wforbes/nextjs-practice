6ccfa2198f708b86aa3d1fe3d4641980cb0e64a5
Will Forbes <wforbes87@gmail.com>
2022-10-15 02:12:23 -0700

    rendering fetched article data on the page
    - playing around with iterating through the article data that was fetched and rendering them out in h3 tags instead of console.log

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,16 @@
 import Head from 'next/head'
 
 export default function Home({ articles }) {
-  console.log(articles)
   return (
     <div>
       <Head>
         <title>Next.js Practice</title>
       </Head>
-      <h1>Welcome to Next</h1>
+      {
+        articles.map(article => 
+          (<h3 key={article.id}>{article.title}</h3>)
+        )
+      }
     </div>
   )
 }
