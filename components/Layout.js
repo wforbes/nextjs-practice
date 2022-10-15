0af5e2799380dd2e69b27b97b0993ba4580f8efe
Will Forbes <wforbes87@gmail.com>
2022-10-15 01:25:15 -0700

    begin creating Layout component
    - components are kept in their own folder, convention is for components have uppercase file names while pages have lowercase
    - repurpose home style sheet to layout style sheet and add it to the new Layout component
    - begin referencing styles from style sheet in the component with this Reacti-esque property decomposition syntax .. className={styles.selector}

---
+++ components/Layout.js
@@ -0,0 +1,13 @@
+import styles from '../styles/Layout.module.js'
+
+const Layout = () => {
+	return (
+		<div className={styles.container}>
+			<main className={styles.main}>
+				
+			</main>
+		</div>
+	)
+}
+
+export default Layout
