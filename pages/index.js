e661116a13b32159c8b344e011b5928afeac45a3
Will Forbes <wforbes87@gmail.com>
2022-10-15 01:17:49 -0700

    add Head element to index
    - add next/head element to the index.js file to test out using this element to add html <head> tag information to the page's markup when the browser renders it

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,10 @@ import Head from 'next/head'
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>Next.js Practice</title>
+        <meta name="keywords" content='web development, cat memes' />
+      </Head>
       <h1>Welcome to Next</h1>
     </div>
   )
