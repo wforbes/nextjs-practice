a26b8aa23d787e4550716644a54e9b0f2c9085fa
Will Forbes <wforbes87@gmail.com>
2022-10-15 02:09:12 -0700

    try fetching data on Home component
    - use getStaticProps to fetch data from jsonplaceholder and console.log them out in the Home component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 
-export default function Home() {
+export default function Home({ articles }) {
+  console.log(articles)
   return (
     <div>
       <Head>
@@ -9,4 +10,21 @@ export default function Home() {
       <h1>Welcome to Next</h1>
     </div>
   )
+}
+
+// 3 different methods can be used to fetch data
+//  1. getStaticProps will fetch at build time
+//  2. getServerSideProps will fetch on every request
+//  3. getStaticPaths will dynamically generate paths based on the data being fetched
+export const getStaticProps = async () => {
+  
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
+
+  const articles = await res.json()
+
+  return {
+    props: {
+      articles
+    }
+  }
 }
